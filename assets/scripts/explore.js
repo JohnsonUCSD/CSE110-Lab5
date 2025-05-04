00f0c297e5c97c37cc218d788176bfe6528877c3
JohnsonUCSD <joc036@ucsd.edu>
2025-05-03 23:50:24 -0700

    Added speech function when button is pressed in explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,16 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const textBox = document.getElementById('text-to-speak');
+  const playButton = document.querySelector('button');
+  const faceImage = document.querySelector('img');
+
+
+  playButton.addEventListener(`click`, () => {
+    let utterance = new SpeechSynthesisUtterance(textBox.value);
+    speechSynthesis.speak(utterance);
+  });
+
+  
 }
