b1493916a149b41139a54621c0f7f21db2f0059a
JohnsonUCSD <joc036@ucsd.edu>
2025-05-06 02:10:22 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,11 +8,53 @@ function init() {
   const playButton = document.querySelector('button');
   const faceImage = document.querySelector('img');
 
+  // Load available voices from Mozilla 
+  const synth = window.speechSynthesis;
+  let voices = [];
 
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // Will text to speech when button is pressed
   playButton.addEventListener(`click`, () => {
-    let utterance = new SpeechSynthesisUtterance(textBox.value);
+    const utterance = new SpeechSynthesisUtterance(textBox.value);
+
+    // Set selected voice
+    const selectedVoiceName = voiceSelect.value;
+    const selectedVoice = voices.find(voice => voice.name === selectedVoiceName);
+    if (selectedVoice) {
+      utterance.voice = selectedVoice;
+    }
+    
+    // Open mouth smiley face when speaking
+    utterance.onstart = function() {
+      faceImage.src = 'assets/images/smiling-open.png'; 
+    };
+    // Change face image back after finish
+    utterance.onend = function() {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+
     speechSynthesis.speak(utterance);
   });
 
-  
 }
