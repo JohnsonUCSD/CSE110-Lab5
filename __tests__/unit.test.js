aa93c880ad878c777911b394e102e513b8de8792
JohnsonUCSD <joc036@ucsd.edu>
2025-05-07 02:20:33 -0700

    testing branch: should be wrong

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -13,7 +13,7 @@ import {
 /* isPhoneNumber Testing */
 describe('isPhoneNumber', () => {
   test('phone number with Area code and paranthesis', () => {
-    expect(isPhoneNumber("(555)555-5555")).toBe(true);
+    expect(isPhoneNumber("(555)555-5555")).4(true);
   });
   test('phone number without area code', () => {
     expect(isPhoneNumber("272-2727")).toBe(true);
